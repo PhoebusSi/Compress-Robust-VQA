4a794b77a190d0f2515665952cf0c582b70db464
Qingyi Si <43233342+PhoebusSi@users.noreply.github.com>
2023-10-12 22:20:40 +0800

    Add files via upload

---
+++ bash_files/Stage2/run_mask_train_stage2_VQAvs.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+cd xxx/VQA/
+echo "langComp$1, VisComp$2, FusComp$3, ZeroRate$4, Seed$5"
+export ROOT_DIR=xxx/VQA/ 
+
+export ZERO_RATE=$4 # # the zero-rate (1- compression ratio) of the whole model
+export lang=$1 # the compression-ratio of language modules
+export vis=$2 # the compression-ratio of vision modules
+export fus=$3 # the compression-ratio of fusion modules
+export seed=$5
+
+
+export model_type=lxmert 
+export label4save=lmh2lmh-VQAvs-Masker-5e-5-linearLR-20epo-$lang-$vis-$fus-zerorate$ZERO_RATE-$seed  
+export training_type=Masker
+export output_dir=xxx/CompressVQA/train_Robust_masker_VQAvs/$label4save/ 
+mkdir -p $output_dir
+
+
+
+python3 $ROOT_DIR/prune_debias_VQAvs.py \
+    --label4save $label4save \
+    --model_type $model_type \
+    --masker_level modal \
+    --Lang_comp $lang \
+    --Vis_comp $vis \
+    --Fus_comp $fus \
+    --FTmodel_type lmh \
+    --Masker_type lmh \
+    --global_prune False \
+    --model_name_or_path $ROOT_DIR/lxmert_config/ \
+    --teacher_model unc-nlp/lxmert-base-uncased \
+    --do_train \
+    --do_eval \
+    --learning_rate 5e-5 \
+    --training_type $training_type \
+    --evaluate_during_training \
+    --output_dir $output_dir \
+    --output_mask_dir $output_dir \
+    --pred_out_dir $output_dir \
+    --logging_dir $output_dir/log/$label4save/logging \
+    --per_gpu_train_batch_size 256 \
+    --per_gpu_eval_batch_size 256 \
+    --num_train_epochs 20 \
+    --logging_steps 100\
+    --save_steps 1802 \
+    --controlled_init magnitude \
+    --zero_rate $ZERO_RATE \
+    --seed $seed \
+    --structured false \
+    --use_kd false 
